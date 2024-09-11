feda3b9a7b11768485c5ad73c5866af953f61fed
Hooman <hooman.rafraf@gmail.com>
2024-09-11 06:27:01 +0330

    trying citrix fixes

--- run_remote.py
+++ run_remote.py
@@ -2,6 +2,6 @@
 import proxy
 if __name__ == '__main__':
     #for VPS use the upper one. For local testing use the lower one
-    with proxy.Proxy(['--hostname', '0.0.0.0', '--port', '9000', '--log-level', 'i', '--num-acceptors', '5', '--num-workers', '10']) as p:
+    with proxy.Proxy(['--hostname', '0.0.0.0', '--port', '9000', '--log-level', 'i', '--num-acceptors', '5', '--num-workers', '10', '--timeout', '2']) as p:
     #with proxy.Proxy(['--port', '9000', '--log-level', 'i']) as p:
         proxy.sleep_loop()
