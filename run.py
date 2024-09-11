6604f1ca8f08d6788b2fa52c6730509c9889dbff
Hooman <hooman.rafraf@gmail.com>
2024-09-11 10:19:44 +0330

    fixed citrix and disconnection problems: set local-executor=0, timeout=200, num acceptors and workers to 16 for VPS instance

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 #This is useful for pyinstaller to create a standalone .exe file out of proxy.py
 
 #local
-import proxy
+import proxy, multiprocessing
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
     with proxy.Proxy(['--plugins', 'proxy.plugin.ProxyPoolPlugin', '--proxy-pool', '77.37.63.119:9000', '--log-level', 'c', '--timeout', '200', '--local-executor', '0']) as p:
         proxy.sleep_loop(p)
